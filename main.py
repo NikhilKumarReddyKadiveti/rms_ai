add4e8343f6677a6ed5186f9f06cd382574b2a10
NikhilKumarReddyKadiveti <sowjanyakadiveti1037@gmail.com>
2026-02-05 13:58:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from ai_model import RMS_AI
@@ -5,13 +6,12 @@ from ai_model import RMS_AI
 app = FastAPI()
 
 ai = RMS_AI(api_key="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")
-
-class ChatRequest(BaseModel):
-    message: str
+class Chat(BaseModel):
+    message:str
 
 @app.post("/chat")
-async def chat(req: ChatRequest):
+async def chat(data:Chat):
 
-    reply = ai.ask(req.message)
+    reply = ai.ask(data.message)
 
-    return {"reply": reply}
+    return {"reply":reply}
